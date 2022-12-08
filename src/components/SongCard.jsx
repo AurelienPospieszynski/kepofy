945e9ffc5aa46a7c9614586f1e2ceb64e46e07fc
AurelienPospieszynski <aurelien.pos@hotmail.fr>
2022-12-08 15:39:56 +0100

    Add titltes subtitles&style to SongCard component

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -1,5 +1,40 @@
-const SongCard = () => (
-  <div>SongCard</div>
-);
+import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { playPause, setActiveSong } from '../redux/features/playerSlice';
+import PlayPause from './PlayPause';
+
+const SongCard = ({ song, i }) => {
+  const activeSong = 'Test';
+  return (
+    <div className="flex flex-col p-4 w-[250px] bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer">
+      <div className="relative w-full h-56 group">
+        <div
+          className={` absolute inset-0 justify-center items-center bg-black bg-opacity-50 group-hover:flex ${
+            activeSong === song.title ? 'flex bg-black bg-opacity-70' : 'hidden'
+          }`}
+        >
+          <PlayPause />
+        </div>
+        <img alt="song_img" src={song.images?.coverart} />
+      </div>
+      <div className="flex flex-col mt-4">
+        <p className="text-lg font-semibold text-white truncate">
+          <Link to={`/songs/${song?.key}`}>{song.title}</Link>
+        </p>
+        <p className="mt-1 text-sm text-gray-300 truncate">
+          <Link
+            to={
+              song.artists
+                ? `/artists/${song?.artists[0]?.adamid}`
+                : '/top-artists'
+            }
+          >
+            {song.subtitle}
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+};
 
 export default SongCard;

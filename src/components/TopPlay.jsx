40dd1a935a3b0ce4ec2fb25a988c186f52cc5ea8
AurelienPospieszynski <aurelien.pos@hotmail.fr>
2022-12-19 13:20:01 +0100

    topplay component starts

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -8,7 +8,15 @@ import { playPause, setActiveSong } from '../redux/features/playerSlice';
 import { useGetTopChartsQuery } from '../redux/services/shazamCore';
 
 import 'swiper/css';
+import 'swiper/css/free-mode';
 
+const TopChartCard = ({ song, i }) => {
+  return (
+    <div className="w-full flex flex-row items-center hover:bg-[#4c426e] py-2 p-4 rounded-lg cursor-pointer mb-2">
+      {song.title}
+    </div>
+  );
+};
 const TopPlay = () => {
   const dispatch = useDispatch();
   const { activeSong, isPlaying } = useSelector((state) => state.player);
@@ -17,7 +25,7 @@ const TopPlay = () => {
 
   useEffect(() => {
     divRef.current.scrollIntoView({ behavior: 'smooth' });
-  }, []);
+  });
 
   const topPlays = data?.slice(0, 5);
 
@@ -33,11 +41,19 @@ const TopPlay = () => {
   return (
     <div
       ref={divRef}
-      className="xl:ml-6 ml-0 mn-0 mb-6 flex-1 xl:max-w-[500px] max-w-full flex flex-col"
+      className="xl:ml-6 ml-0 xl:mb-0 mn-0 mb-6 flex-1 xl:max-w-[500px] max-w-full flex flex-col"
     >
       <div className="flex flex-col w-full">
         <div className="flex flex-row items-center justify-center">
-          <h2>Top charts</h2>
+          <h2 className="text-2xl font-bold text-white">Top charts</h2>
+          <Link to="/top-charts">
+            <p className="text-base text-gray-300 cursor-pointer">See more</p>
+          </Link>
+        </div>
+        <div className="flex flex-col gap-1 mt-4">
+          {topPlays?.map((song, i) => (
+            <TopChartCard key={song.key} song={song} i={i} />
+          ))}
         </div>
       </div>
     </div>

f34a86cd31ac9a4d1f70ef8505c6e0499a9b515b
AurelienPospieszynski <aurelien.pos@hotmail.fr>
2023-01-09 16:40:53 +0100

    starts of songDetailComponent

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -1,3 +1,19 @@
-const SongDetails = () => <div>SongDetails</div>;
+import { useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { DetailsHeader, Error, Loader, RelatedSongs } from '../components';
+import { setActiveSong, playPause } from '../redux/features/playerSlice';
+
+const SongDetails = () => {
+  const dispatch = useDispatch();
+  const { songId } = useParams();
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
+
+  return (
+    <div className="mb-10">
+      <h2 className="text-3xl font-bold text-white">Lyrics: </h2>
+      <div className="mt-5"></div>
+    </div>
+  );
+};
 
 export default SongDetails;
